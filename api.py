b65260bec6160df61d8409a9895bbdcfca3bdc05
Prem <89295807+greenprem@users.noreply.github.com>
2024-03-05 08:40:33 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import nltk
 import requests
 import jwt
-nltk.download('vader_lexicon')
+#nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -11,7 +11,7 @@ sid = SentimentIntensityAnalyzer()
 #constants
 SECRET_KEY = 'greeenprem'
 outj=[]
-times=1
+times=2
 query="cybertruck"
 ex_resp= {}
 url = "https://twitter-api45.p.rapidapi.com/search.php"
@@ -53,6 +53,8 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/sentiment', methods=['GET'])
 def get_sentiment():
+    outj={"data":[],"tweets":""}
+    ex_resp=[]
     auth_header = request.headers.get('Authorization')
     if (not auth_header):
         return jsonify({"error":"Token not in header"})
@@ -68,17 +70,24 @@ def get_sentiment():
         # Perform sentiment analysis or any other operation based on the query
         #fetching from twitter
         for i in range(0,times):
+            print("hello world",query)
+            querystring['query']=query
             response = requests.get(url, headers=headers, params=querystring)
-            ex_resp= response.json()
-            querystring["cursor"]=ex_resp["next_cursor"]
+            ex_resp.append(response.json())
+            print(ex_resp)
+            querystring["cursor"]=ex_resp[i]["next_cursor"]
         #determining scores
-        for i in ex_resp['timeline']:
-            sentiment = determine(i["text"])
-            likes=i['favorites']
-            time=i['created_at']
-            outj.append({"sentiment":sentiment,"likes":likes,"time":time})
+        for req in ex_resp:
+            for i in req['timeline']:
+                sentiment = determine(i["text"])
+                likes=i['favorites']
+                time=i['created_at']
+                outj["tweets"]=outj["tweets"]+i["text"]
+                outj["data"].append({"sentiment":sentiment,"likes":likes,"time":time})
+                print(len(outj))
         # For simplicity, let's just return the query in uppercase
         response = outj
+        outj={"data":[],"tweets":""}
         return jsonify(response)
     except Exception as e:
         error_message = {'error': str(e)}
